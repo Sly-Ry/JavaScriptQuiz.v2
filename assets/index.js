0581ce2e65d784cbad51e91b4212202911ca7e89
ryan <ryan.williams93@aol.com>
2022-06-11 17:05:56 -0500

    added most JS variables for quiz

---
+++ assets/index.js
@@ -0,0 +1,50 @@
+// Variables for page elements
+// time and score
+const timeEl = document.querySelector('p.time');
+const secondsLeft = 5;
+const scoreEl = document.querySelector('#score');
+
+// sections
+// intro section
+const introEl = document.querySelector('#intro');
+
+
+// questions section
+const questionsEl = document.querySelector('#questions');
+
+// questions container
+const questionEl = document.querySelector('#question');
+
+// questions answered count
+const questionCount = 0;
+
+// results div
+const yaynayEl = document.querySelector('#yaynay');
+
+
+// finals section
+const finalEl = document.querySelector('#final');
+
+// user initials
+const initialsInput = document.querySelector('#initials');
+
+
+// highscore section
+const highscoresEl = document.querySelector('#highscores');
+
+// scores list
+const scoresListEl = document/querySelector('#scores-list');
+
+// array of scores
+const scoreList = [];
+
+
+// buttons
+
+// questions
+
+
+
+// Functions
+
+// Event listeners
